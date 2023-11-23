311ebe573d4f3c167e0fb5ab106e4cc021686534
Evie Ciobanu <me@eevie.ro>
2023-11-23 14:29:07 +0200

    socket: move back to original repo after merge

--- lakefile.lean
+++ lakefile.lean
@@ -2,7 +2,7 @@ import Lake
 open Lake DSL
 
 require std from git "https://github.com/leanprover/std4" @ "main"
-require socket from git "https://github.com/eviefp/socket.lean" @ "main"
+require socket from git "https://github.com/hargoniX/socket.lean" @ "main"
 require Parser from git "https://github.com/fgdorais/lean4-parser" @ "main"
 package «todo» { }
 
